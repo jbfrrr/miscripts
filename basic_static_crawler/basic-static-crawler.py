bae69e87b83191aa2b907cbd49106355bf83124b
jbfrrr <jbaferrer@gmail.com>
2017-03-26 22:45:49 +0800

    Catch more possible exceptions for basic-static-crawler.py

--- basic_static_crawler/basic-static-crawler.py
+++ basic_static_crawler/basic-static-crawler.py
@@ -5,15 +5,20 @@ def getUrlStatusCode(url):
         response = requests.head(url)
         return response.status_code
     except requests.ConnectionError:
-        return -1
+        return False
+    except requests.exceptions.MissingSchema:
+        return False
+    except:
+        quit()
 
 def main():
     with open('urls.txt', 'r') as url_file, open('result.txt', 'w') as output:
         for url in url_file:
             print("Accessing %s ...\n" % url)
             url_status_code = getUrlStatusCode(url)
-            output.write("url: %s" % url)
-            output.write("code: %d\n\n" % url_status_code )
+            if (url_status_code):
+                output.write("url: %s" % url)
+                output.write("code: %d\n\n" % url_status_code )
 
 if __name__ == "__main__":
     main()
